38b58b68a8abe621d9ff1bdd3864ac322ee4941b
ashok <ashoknarang2009@gmail.com>
2018-01-28 02:50:44 +0530

    check db issue

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (defined('ENVIRONMENT'))
 // echo ini_get('display_errors');
 
 // if (!ini_get('display_errors')) {
-//     //ini_set('display_errors', '1');
+ini_set('display_errors', '1');
 // }
 
 // echo ini_get('display_errors');
